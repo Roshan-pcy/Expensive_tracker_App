0441bc74f36d848cf760eeb4d7223f7e68adb3c3
roshan pcy <roshan@gmail.com>
2024-05-24 07:42:31 +0530

    added some fucntion

--- expensive_app_2/lib/data/expense_data.dart
+++ expensive_app_2/lib/data/expense_data.dart
@@ -1,3 +1,4 @@
+import 'package:expensive_app_2/datetime/date_time_helper.dart';
 import 'package:expensive_app_2/models/Expensive_Item.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,17 @@ class ExpenseData {
     return overallExpensive_list;
   }
 
+//add into list
   void AddNewExpense(ExpensiveItem expensiveItem) {
     overallExpensive_list.add(expensiveItem);
   }
 
-  //delete expense
+  //delete into list
+  void DeleteExpense(ExpensiveItem expensiveItem) {
+    overallExpensive_list.remove(expensiveItem);
+  }
+
+  //get weekday (mon,tues,ect) from a datetime object
   String getDayName(DateTime datetime) {
     switch (datetime.weekday) {
       case 1:
@@ -26,9 +33,9 @@ class ExpenseData {
         return 'wed';
       case 4:
         return 'thu';
-      case 4:
-        return 'fri';
       case 5:
+        return 'fri';
+      case 6:
         return 'sat';
       case 7:
         return 'sun';
@@ -37,13 +44,21 @@ class ExpenseData {
     }
   }
 
-  //get weekday (mon,tues,ect) from a datetime object
-
   // get the date for the start of the week (sunday)
-  // DateTime startOfWeekDate() {
-  //   DateTime? startOfWeek;
-  //   DateTime today = DateTime.now();
-  // }
+
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+    //get todays date
+    // go backword to today to sunday
+    DateTime today = DateTime.now();
+
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'sun') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+    return startOfWeek!;
+  }
 
   /*convert overall list of expenses into a daily expense summary
 e.g overallExpenseList 
@@ -52,4 +67,22 @@ e.g overallExpenseList
 DailyExpensiveSummary=[[2023/01/30]:$25]
 
   */
+
+  Map<String, double> calculateDailyExpensiveSumanry() {
+    Map<String, double> dailExpenseSumary = {};
+
+    for (var expense in overallExpensive_list) {
+      String date = convertDateTimeToStirng(expense.dateTime);
+      double amount = double.parse(expense.amount);
+
+      if (dailExpenseSumary.containsKey(date)) {
+        double currectAmount = dailExpenseSumary[date]!;
+        currectAmount += amount;
+        dailExpenseSumary[date] = currectAmount;
+      } else {
+        dailExpenseSumary.addAll({date: amount});
+      }
+    }
+    return dailExpenseSumary;
+  }
 }
